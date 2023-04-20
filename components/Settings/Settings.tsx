e8257385f6dd92b862249ffbc6f50308c86ff5e9
Robertas Cekanauskas <rcekanauskasnl@gmail.com>
2023-04-20 21:34:40 +0300

    Add Settings Page

---
+++ components/Settings/Settings.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, Button, Text } from '@mantine/core';
+import { useRouter } from 'next/router';
+
+const Settings = () => {
+  return (
+    <Box>
+      <Text>Settings</Text>
+    </Box>
+  );
+};
+
+export default Settings;

a83a777d37ced6c533f634f16ba3613ffcfab83f
Robertas Cekanauskas <rcekanauskasnl@gmail.com>
2023-04-20 20:54:06 +0300

    Create Mock Dashboard

---
+++ components/Dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppShell from '../AppShell/AppShell';
+import { Text } from '@mantine/core';
+
+function Dashboard() {
+  return <Text>Dashboard</Text>;
+}
+
+export default Dashboard;

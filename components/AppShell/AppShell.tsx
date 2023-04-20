98c9edd76a5da581d025e80d8022a9c41d2d17f6
Robertas Cekanauskas <rcekanauskasnl@gmail.com>
2023-04-20 21:35:18 +0300

    Update App Shell

--- components/AppShell/AppShell.tsx
+++ components/AppShell/AppShell.tsx
@@ -1,90 +1,9 @@
-import {
-  Text,
-  AppShell as AppShellMantine,
-  Navbar,
-  Header,
-  Footer,
-  Stack,
-  Aside,
-  Box,
-  Group,
-  Divider,
-  Button,
-} from '@mantine/core';
+import { AppShell as AppShellMantine } from '@mantine/core';
 import React from 'react';
-import { ColorSchemeToggle } from '../ColorSchemeToggle/ColorSchemeToggle';
-import {
-  IconBrandFoursquare,
-  IconDashboard,
-  IconHelp,
-  IconLayoutGrid,
-  IconSettings,
-  IconSquareF1,
-  IconSquareRoot,
-  IconSquaresDiagonal,
-  IconSquaresFilled,
-} from '@tabler/icons';
-import { IconSquareRoundedLetterS } from '@tabler/icons-react';
+import Navbar from '../Navbar/Navbar';
 
 const AppShell = ({ children }: { children: React.ReactNode }) => (
-  <AppShellMantine
-    navbarOffsetBreakpoint="sm"
-    asideOffsetBreakpoint="sm"
-    navbar={
-      <Navbar px="sm" hiddenBreakpoint="sm" py="md" width={{ sm: 150, lg: 250 }}>
-        <Group position="apart">
-          <Text weight="bold" size="xl">
-            DeFi Dashboard
-          </Text>
-          <ColorSchemeToggle />
-        </Group>
-        <Divider mt="sm" mb="xl" orientation="horizontal" />
-        <Button.Group orientation="vertical" sx={{ rowGap: '10px' }}>
-          {/*  */}
-          <Button
-            fullWidth={true}
-            sx={{ alignItems: 'flex-start' }}
-            leftIcon={<IconLayoutGrid />}
-            styles={{
-              inner: {
-                justifyContent: 'flex-start',
-              },
-            }}
-            variant="light"
-            size="lg"
-          >
-            Dashboard
-          </Button>
-          <Button
-            styles={{
-              inner: {
-                justifyContent: 'flex-start',
-              },
-            }}
-            fullWidth={true}
-            leftIcon={<IconHelp />}
-            variant="light"
-            size="lg"
-          >
-            Documentation
-          </Button>
-          <Button
-            styles={{
-              inner: {
-                justifyContent: 'flex-start',
-              },
-            }}
-            fullWidth={true}
-            leftIcon={<IconSettings />}
-            variant="light"
-            size="lg"
-          >
-            Settings
-          </Button>
-        </Button.Group>
-      </Navbar>
-    }
-  >
+  <AppShellMantine navbarOffsetBreakpoint="sm" asideOffsetBreakpoint="sm" navbar={<Navbar />}>
     {children}
   </AppShellMantine>
 );

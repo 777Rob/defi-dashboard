539116cee5f7c43fb07923f3f6a4da41429974db
Robertas Cekanauskas <rcekanauskasnl@gmail.com>
2023-04-21 00:11:22 +0300

    Create Chart component

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import AppShell from '../AppShell/AppShell';
-import { Box, Container, Paper, Text } from '@mantine/core';
+import { Box, Container, Grid, Paper, Skeleton, Text } from '@mantine/core';
 import { gql, useQuery } from '@apollo/client';
 import usePancakeDayDataBSC from '../../hooks/usePancakeDayDataBSC';
 
+const PancakeChartBSC = () => {
+  const { loading, error, data } = usePancakeDayDataBSC();
+
+  return (
+    <Paper shadow="lg" p="md">
+      <Text weight={700} pb="sm" sx={{ textAlign: 'center' }} size="lg">
+        Pancake Swap Trading Trend Diagram{' '}
+      </Text>
+      <Skeleton
+        sx={{ width: '100%', height: '100%' }}
+        mih={350}
+        p="lg"
+        visible={loading}
+      ></Skeleton>
+    </Paper>
+  );
+};
+
 function Dashboard() {
   const { loading, error, data } = usePancakeDayDataBSC();
+
   console.log(data);
   return (
     <Box>
-      <Paper shadow="lg" w="50%" h="400px">
-        <Text weight={700} size="lg" p="md">
-          Pancake Swap Trading Trend Diagram{' '}
-        </Text>
-      </Paper>
+      <Grid>
+        <Grid.Col span={6}>
+          <PancakeChartBSC />
+        </Grid.Col>
+      </Grid>
     </Box>
   );
 }

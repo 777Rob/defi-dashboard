25fe2ae89ee853385837a2efdb33431069c4c406
Robertas Cekanauskas <rcekanauskasnl@gmail.com>
2023-04-22 15:22:50 +0300

    Update utils

--- utils/index.ts
+++ utils/index.ts
@@ -25,7 +25,6 @@ export const generateTicks = (data: any) => {
 export const customRound = (value: any) => {
   try {
     let precision;
-    console.log(value);
     if (Math.floor(value) !== 0) {
       precision = 2;
     } else {

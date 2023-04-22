457cc672073006f027015731176bfdcef195b03e
Robertas Cekanauskas <rcekanauskasnl@gmail.com>
2023-04-22 16:15:51 +0300

    Display statistics

--- components/Dashboard/PancakeDetails.tsx
+++ components/Dashboard/PancakeDetails.tsx
@@ -1,25 +1,53 @@
-import { Paper, Card } from '@mantine/core';
+import { Paper, Grid, Box, Button } from '@mantine/core';
 import React from 'react';
 import { PancakeTextIcon } from '../icons';
 import usePancakeDetails from 'hooks/usePancakeDetails';
+import { displayNumber } from '../../utils/index';
+import StatisticCard from './StatisticCard';
 
 const PancakeDetails = () => {
   const { data, loading, error } = usePancakeDetails();
   console.log(data, loading, error);
+
   return (
-    <Card>
-      <PancakeTextIcon width="40%" />
-      {/* <Text
-          weight={700}
-          pb="sm"
-          display="flex"
-          align="center"
-          sx={{ alignItems: 'center', columnGap: '10px' }}
-          size="xl"
-          >
-          Binance Smart Chain <BinanceIcon width={35} />
-        </Text> */}
-    </Card>
+    <Grid>
+      <Grid.Col span={3}>
+        <StatisticCard
+          loading={loading}
+          title="Volume USD (24h)"
+          tooltip="Total amount of USD traded on the exchange in the past 24 hours"
+        >
+          ${displayNumber(data?.dailyVolumeUSD)}
+        </StatisticCard>
+      </Grid.Col>
+      <Grid.Col span={3}>
+        <StatisticCard
+          loading={loading}
+          title="Total Liquidity USD"
+          tooltip="Total amount of USD currently locked in the exchange"
+        >
+          ${displayNumber(data?.totalLiquidityUSD)}
+        </StatisticCard>
+      </Grid.Col>
+      <Grid.Col span={3}>
+        <StatisticCard
+          loading={loading}
+          title="Volume USD (All Time)"
+          tooltip="Total amount of $USD traded on the exchange in the past 24 hours"
+        >
+          ${displayNumber(data?.totalVolumeUSD)}
+        </StatisticCard>
+      </Grid.Col>
+      <Grid.Col span={3}>
+        <StatisticCard
+          loading={loading}
+          title="Volume USD (24h)"
+          tooltip="Total amount of $USD traded on the exchange in the past 24 hours"
+        >
+          ${displayNumber(data?.dailyVolumeUSD)}
+        </StatisticCard>
+      </Grid.Col>
+    </Grid>
   );
 };
 

21aca1a1bc432dc1c9b96ec757989b8af2e259a4
Robertas Cekanauskas <rcekanauskasnl@gmail.com>
2023-05-15 08:09:09 +0300

    Fix

--- apollo/index.ts
+++ apollo/index.ts
@@ -2,14 +2,14 @@ import { ApolloClient, InMemoryCache, ApolloProvider, gql, HttpLink } from '@apo
 
 export const bscClient = new ApolloClient({
   link: new HttpLink({
-    uri: process.env.NEXT_PUBLIC_BSC_GRAPHQL_URL,
+    uri: 'https://open-platform.nodereal.io/d05b7aedbde943dfb6c533fe4f6909cd/pancakeswap-free/graphql',
   }),
   cache: new InMemoryCache(),
 });
 
 export const ethClient = new ApolloClient({
   link: new HttpLink({
-    uri: process.env.NEXT_PUBLIC_ETH_GRAPHQL_URL,
+    uri: 'https://api.thegraph.com/subgraphs/name/pancakeswap/exchange-v3-eth',
   }),
   cache: new InMemoryCache(),
 });
